92c2174022422d5ae10973a6e860fd5fa420cf0d
isakulaksiz <isakulaksiz@outlook.com>
2022-03-18 21:13:28 +0300

    initialized project

---
+++ main.py
@@ -0,0 +1,13 @@
+""""
+    Created by Software Engineer Isa Kulaksiz
+    Created time 18.03.2022 / dd.mm.yyyy
+"""
+
+
+def print_hi(name):
+
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
