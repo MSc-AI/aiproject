a23ca8b48679db6e73a72cd42492e99441f399e1
isakulaksiz <isakulaksiz@outlook.com>
2022-03-20 22:34:20 +0300

    feat: calculated validation model

--- ValidationModel.py
+++ ValidationModel.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 import seaborn as sns
@@ -25,31 +26,31 @@ def validation(self):
 
     print("TRAIN DATA")
     # 0 ->  gynecology / 1 -> anesthesia / 2-> radiotherapy / 3 -> TB & Chest disease / 4 -> surgery
-    # print(df["Department"].value_counts())
+    print(df["Department"].value_counts())
     df_copy_test = df_copy_test.replace(['gynecology'], '0')
     df_copy_test = df_copy_test.replace(['anesthesia'], '1')
     df_copy_test = df_copy_test.replace(['radiotherapy'], '2')
     df_copy_test = df_copy_test.replace(['TB & Chest disease'], '3')
     df_copy_test = df_copy_test.replace(['surgery'], '4')
-    # print(df_copy_test["Department"].value_counts())
+    print(df_copy_test["Department"].value_counts())
 
     # 0 -> Moderate / 1 -> Minor / 2 -> Extreme / 3 -> Severity of Illness
-    # print(df["Severity of Illness"].value_counts())
+    print(df["Severity of Illness"].value_counts())
     df_copy_test = df_copy_test.replace(['Moderate'], '0')
     df_copy_test = df_copy_test.replace(['Minor'], '1')
     df_copy_test = df_copy_test.replace(['Extreme'], '2')
-    # print(df_copy_test["Severity of Illness"].value_counts())
+    print(df_copy_test["Severity of Illness"].value_counts())
 
     # 0 -> Trauma / 1 -> Emergency / 2 -> Urgent
-    # print(df["Type of Admission"].value_counts())
+    print(df["Type of Admission"].value_counts())
     df_copy_test = df_copy_test.replace(['Trauma'], '0')
     df_copy_test = df_copy_test.replace(['Emergency'], '1')
     df_copy_test = df_copy_test.replace(['Urgent'], '2')
-    # print(df_copy_test["Type of Admission"].value_counts())
+    print(df_copy_test["Type of Admission"].value_counts())
 
     # 0 -> 41-50 / 1 -> 31-40 / 2 -> 51-60 / 3 -> 21-30 / 4 -> 71-80 / 5 -> 61-70
     # / 6 -> 11-20 / 7 -> 81-90 / 8 -> 0-10 / 9 -> 91-100
-    # print(df["Age"].value_counts())
+    print(df["Age"].value_counts())
     df_copy_test = df_copy_test.replace(['41-50'], '0')
     df_copy_test = df_copy_test.replace(['31-40'], '1')
     df_copy_test = df_copy_test.replace(['51-60'], '2')
@@ -60,11 +61,11 @@ def validation(self):
     df_copy_test = df_copy_test.replace(['81-90'], '7')
     df_copy_test = df_copy_test.replace(['0-10'], '8')
     df_copy_test = df_copy_test.replace(['91-100'], '9')
-    # print(df_copy_test["Age"].value_counts())
+    print(df_copy_test["Age"].value_counts())
 
     # 0 -> 21-30 / 1 -> 11-20 / 2 -> 31-40 / 3 -> 51-60 / 4 -> 0-10 / 5 -> 41-50
     # 6 -> 71-80 / 7 -> More than 100 Days /  8 -> 81-90 / 9 -> 91-100 / 10 -> 61-70
-    # print(df["Stay"].value_counts())
+    print(df["Stay"].value_counts())
     df_copy_test = df_copy_test.replace(['21-30'], '0')
     df_copy_test = df_copy_test.replace(['11-20'], '1')
     df_copy_test = df_copy_test.replace(['31-40'], '2')
@@ -76,7 +77,7 @@ def validation(self):
     df_copy_test = df_copy_test.replace(['81-90'], '8')
     df_copy_test = df_copy_test.replace(['91-100'], '9')
     df_copy_test = df_copy_test.replace(['61-70'], '10')
-    # print(df_copy_test["Stay"].value_counts())
+    print(df_copy_test["Stay"].value_counts())
 
     x_train = df_copy_test[
         ["Hospital_code", "patientid", "Department", "Age", "Severity of Illness", "Type of Admission"]]
@@ -130,7 +131,39 @@ def validation(self):
     dt = DecisionTreeClassifier(criterion="entropy", random_state=1234, max_depth=4, min_samples_split=4)
     model = dt.fit(x_train, y_train)
 
+    # %25 validation data
+    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,
+                                                      test_size=0.25,
+                                                      shuffle=False)
+
     y_train = y_train[0:len(x_test):]
+
+    prediction = dt.predict(x_test)
+    accuracy = accuracy_score(y_train, prediction)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    # print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(y_train, prediction))
+    # print("Report :" + '\n', classification_report(y_train, prediction))
+    print(model)
+
+    # validation process
+    prediction = dt.predict(x_val)
+
+    #### It is the combination of forecast results and real data on a dataframe.
+    # prediction = pd.DataFrame(prediction, ['prediction'])
+    # prediction['index'] = range(1, len(prediction) + 1)
+    # y_val = y_val.reset_index()
+    # y_val.drop(columns=['index'])
+    # y_val['index'] = range(1, len(y_val) + 1)
+    # valid = pd.merge(prediction, y_val, on='index', how='left')
+
+    print("\r\n_______________________________________________________\r\n")
+    # accuracy = accuracy_score(valid['Stay'], valid['prediction'])
+    # print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    # print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(valid['Stay'], valid['prediction']))
+    accuracy = accuracy_score(y_val, prediction)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(y_val, prediction))
+
 
     # Creating the dataset
     column_name = ['Hospital_code', 'patientid', 'Department',
@@ -142,20 +175,6 @@ def validation(self):
     y = np.array([50, 200, 1000, 1500, 2000, 2500])
 
     plt.bar(x, y)
-    plt.show()
-
-    """x_train, x_val, y_train, y_val = train_test_split(x_train, y_train,
-                                                      test_size=1.0,
-                                                      shuffle=False)"""
-
-    prediction = dt.predict(x_test)
-    accuracy = accuracy_score(y_train, prediction)
-    print("Accuracy: %.2f%%" % (accuracy * 100.0))
-    print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(y_train, prediction))
-    print("Report :" + '\n', classification_report(y_train, prediction))
-    print(model)
-
-
-
+    # plt.show()
 
     return df
