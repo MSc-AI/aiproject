72d0eb1ce6d9b364d30e260914153187853a502e
isakulaksiz <isakulaksiz@outlook.com>
2022-04-14 20:43:54 +0300

    feat: calc test data via decision tree changed understandable variable name

--- ValidationModel.py
+++ ValidationModel.py
@@ -21,80 +21,80 @@ df_test = ReadCSV.FileOperations.read_test_file(self)
 
 def feature_selection():
     # print(df.dtypes)
-    df_copy_test = df[
+    df_train = df[
         ["Hospital_code", "patientid", "Department", "Age", "Severity of Illness", "Type of Admission", "Stay"]].copy()
-    print(df_copy_test.value_counts())
+    print(df_train.value_counts())
     # print("TRAIN DATA")
-    print(df_copy_test.head(20))
+    print(df_train.head(20))
     # 0 ->  gynecology / 1 -> anesthesia / 2-> radiotherapy / 3 -> TB & Chest disease / 4 -> surgery
     # print(df["Department"].value_counts())
-    df_copy_test = df_copy_test.replace(['gynecology'], '0')
-    df_copy_test = df_copy_test.replace(['anesthesia'], '1')
-    df_copy_test = df_copy_test.replace(['radiotherapy'], '2')
-    df_copy_test = df_copy_test.replace(['TB & Chest disease'], '3')
-    df_copy_test = df_copy_test.replace(['surgery'], '4')
-    # print(df_copy_test["Department"].value_counts())
+    df_train = df_train.replace(['gynecology'], '0')
+    df_train = df_train.replace(['anesthesia'], '1')
+    df_train = df_train.replace(['radiotherapy'], '2')
+    df_train = df_train.replace(['TB & Chest disease'], '3')
+    df_train = df_train.replace(['surgery'], '4')
+    # print(df_train["Department"].value_counts())
 
     # 0 -> Moderate / 1 -> Minor / 2 -> Extreme / 3 -> Severity of Illness
     # print(df["Severity of Illness"].value_counts())
-    df_copy_test = df_copy_test.replace(['Moderate'], '0')
-    df_copy_test = df_copy_test.replace(['Minor'], '1')
-    df_copy_test = df_copy_test.replace(['Extreme'], '2')
-    # print(df_copy_test["Severity of Illness"].value_counts())
+    df_train = df_train.replace(['Moderate'], '0')
+    df_train = df_train.replace(['Minor'], '1')
+    df_train = df_train.replace(['Extreme'], '2')
+    # print(df_train["Severity of Illness"].value_counts())
 
     # 0 -> Trauma / 1 -> Emergency / 2 -> Urgent
     # print(df["Type of Admission"].value_counts())
-    df_copy_test = df_copy_test.replace(['Trauma'], '0')
-    df_copy_test = df_copy_test.replace(['Emergency'], '1')
-    df_copy_test = df_copy_test.replace(['Urgent'], '2')
-    # print(df_copy_test["Type of Admission"].value_counts())
+    df_train = df_train.replace(['Trauma'], '0')
+    df_train = df_train.replace(['Emergency'], '1')
+    df_train = df_train.replace(['Urgent'], '2')
+    # print(df_train["Type of Admission"].value_counts())
 
     # 0 -> 41-50 / 1 -> 31-40 / 2 -> 51-60 / 3 -> 21-30 / 4 -> 71-80 / 5 -> 61-70
     # / 6 -> 11-20 / 7 -> 81-90 / 8 -> 0-10 / 9 -> 91-100
     # print(df["Age"].value_counts())
-    df_copy_test = df_copy_test.replace(['41-50'], '0')
-    df_copy_test = df_copy_test.replace(['31-40'], '1')
-    df_copy_test = df_copy_test.replace(['51-60'], '2')
-    df_copy_test = df_copy_test.replace(['21-30'], '3')
-    df_copy_test = df_copy_test.replace(['71-80'], '4')
-    df_copy_test = df_copy_test.replace(['61-70'], '5')
-    df_copy_test = df_copy_test.replace(['11-20'], '6')
-    df_copy_test = df_copy_test.replace(['81-90'], '7')
-    df_copy_test = df_copy_test.replace(['0-10'], '8')
-    df_copy_test = df_copy_test.replace(['91-100'], '9')
-    # print(df_copy_test["Age"].value_counts())
+    df_train = df_train.replace(['41-50'], '0')
+    df_train = df_train.replace(['31-40'], '1')
+    df_train = df_train.replace(['51-60'], '2')
+    df_train = df_train.replace(['21-30'], '3')
+    df_train = df_train.replace(['71-80'], '4')
+    df_train = df_train.replace(['61-70'], '5')
+    df_train = df_train.replace(['11-20'], '6')
+    df_train = df_train.replace(['81-90'], '7')
+    df_train = df_train.replace(['0-10'], '8')
+    df_train = df_train.replace(['91-100'], '9')
+    # print(df_train["Age"].value_counts())
 
     # 0 -> 21-30 / 1 -> 11-20 / 2 -> 31-40 / 3 -> 51-60 / 4 -> 0-10 / 5 -> 41-50
     # 6 -> 71-80 / 7 -> More than 100 Days /  8 -> 81-90 / 9 -> 91-100 / 10 -> 61-70
     # print(df["Stay"].value_counts())
-    df_copy_test = df_copy_test.replace(['21-30'], '0')
-    df_copy_test = df_copy_test.replace(['11-20'], '1')
-    df_copy_test = df_copy_test.replace(['31-40'], '2')
-    df_copy_test = df_copy_test.replace(['51-60'], '3')
-    df_copy_test = df_copy_test.replace(['0-10'], '4')
-    df_copy_test = df_copy_test.replace(['41-50'], '5')
-    df_copy_test = df_copy_test.replace(['71-80'], '6')
-    df_copy_test = df_copy_test.replace(['More than 100 Days'], '7')
-    df_copy_test = df_copy_test.replace(['81-90'], '8')
-    df_copy_test = df_copy_test.replace(['91-100'], '9')
-    df_copy_test = df_copy_test.replace(['61-70'], '10')
-    # print(df_copy_test["Stay"].value_counts())
-    return df_copy_test
+    df_train = df_train.replace(['21-30'], '0')
+    df_train = df_train.replace(['11-20'], '1')
+    df_train = df_train.replace(['31-40'], '2')
+    df_train = df_train.replace(['51-60'], '3')
+    df_train = df_train.replace(['0-10'], '4')
+    df_train = df_train.replace(['41-50'], '5')
+    df_train = df_train.replace(['71-80'], '6')
+    df_train = df_train.replace(['More than 100 Days'], '7')
+    df_train = df_train.replace(['81-90'], '8')
+    df_train = df_train.replace(['91-100'], '9')
+    df_train = df_train.replace(['61-70'], '10')
+    # print(df_train["Stay"].value_counts())
+    return df_train
 
 
 def feature_extraction():
-    df_copy_test = feature_selection()
+    df_copy_train = feature_selection()
     options_sol = ['2']
-    rslt_df = df_copy_test.loc[df_copy_test['Severity of Illness'].isin(options_sol)]
+    rslt_df = df_copy_train.loc[df_copy_train['Severity of Illness'].isin(options_sol)]
     print('\nResult Severity of Illness :\n',
           rslt_df)
 
     options_age = ['4', '5', '7', '9']
-    rslt_df_age = df_copy_test.loc[df_copy_test['Age'].isin(options_age)]
+    rslt_df_age = df_copy_train.loc[df_copy_train['Age'].isin(options_age)]
     print('\nResult Age :\n',
           rslt_df_age)
 
-    df_feature_ext = df_copy_test.copy()
+    df_feature_ext = df_copy_train.copy()
     print("rslt_df size:" + str(rslt_df.shape))
     common = rslt_df.merge(rslt_df_age, left_index=True, right_index=True, how='outer', suffixes=('', '_drop'))
     common.drop(common.filter(regex='_y$').columns.tolist(), axis=1, inplace=False)
@@ -116,13 +116,13 @@ def feature_extraction():
         if common["Hospital_code"][i] == "0" and common["patientid"][i] == "0" and common["Department"][i] == "0" and \
                 common["Age"][i] == "0" and common["Severity of Illness"][i] == "0" and common["Type of Admission"][
             i] == "0" and common["Stay"][i] == "0":
-            row["Hospital_code"] = df_copy_test["Hospital_code"][i]
-            row["patientid"] = df_copy_test["patientid"][i]
-            row["Department"] = df_copy_test["Department"][i]
-            row["Age"] = df_copy_test["Age"][i]
-            row["Severity of Illness"] = df_copy_test["Severity of Illness"][i]
-            row["Type of Admission"] = df_copy_test["Type of Admission"][i]
-            row["Stay"] = df_copy_test["Stay"][i]
+            row["Hospital_code"] = df_copy_train["Hospital_code"][i]
+            row["patientid"] = df_copy_train["patientid"][i]
+            row["Department"] = df_copy_train["Department"][i]
+            row["Age"] = df_copy_train["Age"][i]
+            row["Severity of Illness"] = df_copy_train["Severity of Illness"][i]
+            row["Type of Admission"] = df_copy_train["Type of Admission"][i]
+            row["Stay"] = df_copy_train["Stay"][i]
 
             # row["priority"] = "NO"
             row["priority"] = "0"
@@ -285,6 +285,7 @@ def validation(self):
     x_test_feat = df_test_common[
         ["Hospital_code", "patientid", "Department", "Age", "Severity of Illness", "Type of Admission", "priority"]]
 
+
     dt = DecisionTreeClassifier()
     model = dt.fit(x_train_feat, y_train_feat)
     print(model)
@@ -305,6 +306,27 @@ def validation(self):
     print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(y_train_feat, prediction_))
     # print("Report :" + '\n', classification_report(y_train_feat, prediction))
     print("\r\n____________________________________________________\r\n")
+
+    dt = DecisionTreeClassifier()
+    y_train_feat = y_train_feat[0:len(x_test_feat):]
+    model = dt.fit(x_test_feat, y_train_feat)
+    print(model)
+
+    # %25 validation data
+    x_test_feat, x_val_test, y_train_feat, y_val_test = train_test_split(x_test_feat, y_train_feat,
+                                                                         test_size=0.40,
+                                                                         shuffle=False)
+
+    prediction_ = dt.predict(x_test_feat)
+    accuracy = accuracy_score(y_train_feat, prediction_)
+    print("\r\n____________________________________________________\r\n")
+    print("TEST DATA:")
+    print("\r\n____________________________________________________\r\n")
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    print('\n' + "Confusion Matrix: " + '\n', confusion_matrix(y_train_feat, prediction_))
+    # print("Report :" + '\n', classification_report(y_train_feat, prediction))
+    print("\r\n____________________________________________________\r\n")
+
 
     """dot_data = StringIO()
     clf = model
